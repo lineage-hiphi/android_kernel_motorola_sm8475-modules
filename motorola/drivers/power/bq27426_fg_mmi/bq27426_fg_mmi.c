73dba8b9051809ca4c2615fad0c6b50f0c42a34d
bigsupersquid <0.uradork.0@gmail.com>
2026-09-18 05:19:29 -0500

    bq27426_fg_mmi: error: variable 'ret' set but not used [-Werror,-Wunused-but-set-variable]
    Change-Id: Icd7c2d784ca483a5f926c39ccc125c77d6236373

--- motorola/drivers/power/bq27426_fg_mmi/bq27426_fg_mmi.c
+++ motorola/drivers/power/bq27426_fg_mmi/bq27426_fg_mmi.c
@@ -2215,7 +2215,7 @@ static void fg_update_bqfs_workfunc(struct work_struct *work)
 static void fg_dump_registers(struct bq_fg_chip *bq)
 {
 	int i;
-	int ret;
+	int ret __maybe_unused;
 	u16 val;
 
 	for (i = 0; i < ARRAY_SIZE(fg_dump_regs); i++) {
